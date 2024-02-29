73a8d8a46a06a5db51560ff1b81e56c3e637bd9a
Angel-Jang <161417720+Angel-Jang@users.noreply.github.com>
2024-02-29 14:20:28 +0800

    Update JPA103.java

--- JPA103.java
+++ JPA103.java
@@ -5,11 +5,11 @@ public class JPA103 {
     public static void main(String[] args) {
         System.out.println("Please input:");
         Scanner scn = new Scanner(System.in);
-        byte x,y,z;
+        int x,y,z;
 
-        x = scn.nextByte();
-        y = scn.nextByte();
-        z = scn.nextByte();
+        x = scn.nextInt();
+        y = scn.nextInt();
+        z = scn.nextInt();
 
         System.out.printf("Average: %4.2f", ((float)(x+y+z)/3));
         System.out.println();
