bb98ec9331b0d24382d8da540339a68bc07c1967
Angel-Jang <161417720+Angel-Jang@users.noreply.github.com>
2024-03-05 06:46:20 +0800

    Update JPA104SE.java

--- JPA104SE.java
+++ JPA104SE.java
@@ -14,11 +14,7 @@ public class JPA104SE {
 
         float distance = (float)Math.sqrt(Math.pow(point2.getX() - point1.getX(), 2) + Math.pow(point2.getY() - point1.getY(), 2));
         
-        System.out.printf("介於(%.2f", point1.getX());
-        System.out.printf(",%.2f", point1.getY());
-        System.out.printf(")和(%.2f", point2.getX());
-        System.out.printf(",%.2f", point2.getY());
-        System.out.printf(")之間的距離是%.2f", distance);
+        System.out.printf("介於(%.2f,%.2f)和(%.2f,%.2f)之間的距離是%.2f", point1.getX(), point1.getY(), point2.getX(), point2.getY(),distance);
         System.out.println();
         scn.close();
 
